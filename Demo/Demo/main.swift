ab9ad6c05a817065bb01fd76d9d1dbe9e6fb9d01
Noah Gift <noah.gift@gmail.com>
2022-01-10 08:54:43 -0500

    adding hello world swift

---
+++ Demo/Demo/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Demo
+//
+//  Created by Noah Gift on 1/10/22.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ hello.swift
@@ -0,0 +1,5 @@
+#!/usr/bin/env swift
+
+import Foundation
+
+print("Hello, World!")
